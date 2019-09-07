4b6ebfa27cf655ca79737c0f789bfae8b0a13c0d
João André Lopes Carias <carias.joao@gmail.com>
2019-09-07 20:58:00 +0100

    Added SideMenu component.
    Signed-off-by: João André Lopes Carias <carias.joao@gmail.com>

---
+++ front-end/src/components/SideMenu/SideMenu.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./SideMenu.css";
+const SideMenu = props => {
+  return (
+    <div className="side-menu-root">
+      <span>This is the menu</span>
+    </div>
+  );
+};
+
+export default SideMenu;
